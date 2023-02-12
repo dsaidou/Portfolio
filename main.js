fb4b7f3c0ef6c59c66fa48a07824d13d06241eef
dsaidou <saidoud818@gmail.com>
2023-02-12 15:59:17 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// let navbarLink = document.getElementsByClassName("navbarLink");
+// let navbarElement = document.getElementsByClassName("navbarElement");
+
+// for(i = 0; i < navbarLink.length; i++) {
+//     navbarLink[0].addEventListener("mouseover", () => {
+//         for(j = 0; navbarElement.length; j++) {
+//             navbarElement[0].style.width = "100%";
+//         }
+//     })
+
+// }
